547d19eff0d6fcdcbf206ab78c260abc3779bfbd
Pas-nou <pascal.renou@live.fr>
2024-05-18 19:55:26 +0200

    add animation on the fox

--- src/Components/FoxComponent.jsx
+++ src/Components/FoxComponent.jsx
@@ -5,18 +5,27 @@ import Fox from '../../public/Fox'
 
 function foxComponent() {
 
-    return (
-        <>
-        <Canvas>
+  return (
+    <>
+      <Canvas>
         <ambientLight intensity={2} />
-        <OrbitControls />
+        <OrbitControls
+          enableZoom={false}
+          rotateSpeed={2}
+          autoRotate={true}
+          autoRotateSpeed={0.25}
+        />
         <Suspense fallback={null}>
-          <Fox />
-        </Suspense>
+          <Fox 
+          position={[-3, -2, -3]}
+          // rotation={[Math.PI / -0.1, -0.5, 0.5]}
+          scale={[0.5, 0.5, 0.5]}
+        />
+          </Suspense>
         <Environment preset='sunset' />
       </Canvas>
-        </>
-    )
+    </>
+  )
 }
 
 export default foxComponent;
